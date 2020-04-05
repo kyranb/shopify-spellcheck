7a3b30bf5e9b35d9b8051421a2034b34baf77637
Kyran <kyranb@users.noreply.github.com>
2020-04-05 12:50:25 +1000

    WIP

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/proxy', function () {
+    return response('Hello, world!')->withHeaders(['Content-Type' => 'application/liquid']);
+})->middleware('auth.proxy');
+
 Route::get('/', function () {
+    $shop = Auth::user();
+    $request = $shop->api()->rest('GET', '/admin/api/products.json');
+    $products = collect($request->body->products);
+
+    // Upsert all products based upon product id
+    dd($products);
+
+    Log::info('Imported products for store: '.json_encode($shop));
+    
+        
     return view('welcome');
-});
+})->middleware(['auth.shopify'])->name('home');
+
+
+Route::get('shopify', 'ShopifyController@index')->middleware(['auth.shopify']);
